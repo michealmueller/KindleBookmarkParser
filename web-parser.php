9d55f1a6d119dbed8dd76066725972085479cadd
Micheal <arthmael86@gmail.com>
2013-08-01 00:37:00 -0400

    update the bad looking UI with a new one using the twitter bootstrap

--- web-parser.php
+++ web-parser.php
@@ -10,23 +10,19 @@
 <html>
 <head>
     <title>Snippet Parser Web GUI</title>
-    <LINK href="css/global.css" rel="stylesheet" type="text/css">
+
+    <LINK href="css/bootstrap.css" rel="stylesheet" type="text/css">
 </head>
 <body>
-<div id="content" class="shadow">
-    <div class="header">
-        <p>
-            <h3>Snippet Parser Web GUI</h3>
-            <ol type="none">
-                <li>First Choose the Input File</li>
-                <li>Type in the output file name, i.e test.html</li>
-                <li>Click submit, if you did not set the static location then the file will be placed in the same directory as the script.</li>
-            </ol>
-        </p>
+<div class="content">
+    <div class="page-header">
+         <h3>Snippet Parser Web GUI</h3>
+                1.First Choose the Input File<br />
+                2.Type in the output file name, i.e test.html<br />
+                3.Click submit, if you did not set the static location then the file will be placed in the same directory as the script.<br />
     </div>
-    <div class="warnin-msg">
-        <p>
-            <b>Be SURE you have read/write access to the directory you select to store the HTML files in.</b>
+    <div class="text-danger text-center">
+        <b>Be SURE you have read/write access to the directory you select to store the HTML files in.</b>
             <br>
             <?php
                 if($configWork->detectConfig() == false)
@@ -34,48 +30,47 @@
                     echo 'The config file has not been created, so the file will be saved in the current working directory(' . getcwd() .'), if you do not want this please set the directory on the right.';
                 }
             ?>
-        </p>
     </div>
-    <div class="uploadForm">
-        <form action="webToApp.php" method="post">
-            <table align="center">
+    <div class="pull-left">
+            <form action="webToApp.php" method="post" class="form-inline">
+            <table class="table">
                 <tr>
-                    <td>Snippet File:</td>
+                    <td><label for="input" >Snippet File:</label></td>
                 </tr>
                 <tr>
-                    <td><input type="file" name="input" value="Browse" /></td>
+                    <td><input class="form-control btn-default btn-mini" type="file" name="input" value="Browse" /></td>
                 </tr>
                 <tr>
-                    <td>Output File:</td>
+                    <td><label for="output">Output File:</label></td>
                 </tr>
                 <tr>
                     <td class="filepath">
                     	<?php
                     		$configWork -> detectConfig();
-	                      	echo $configWork -> savedDir;
+	                      	echo $configWork -> savedDir.'/';
                         ?>
                     </td>
                 </tr>
                 <tr>
-                	<td><input type="text" name="output"></td>
+                	<td><input class="form-control input-small" type="text" name="output" placeholder="Enter File Name"></td>
                 </tr>
                 <tr>
-                    <td colspan="2"><input type="submit" name="submit" value="Submit" ></td>
+                    <td colspan="2"><input class="btn btn-primary btn-small" type="submit" name="submit" value="Submit" ></td>
                 </tr>
             </table>
         </form>
     </div>
-    <div class="settings">
-        <form action="saveLoc.php" method="post">
-            <table>
+    <div class="pull-right">
+           <form action="saveLoc.php" method="post">
+            <table class="table">
                 <tr>
-                    <td>If you would like to set a static location to put all the HTML output files,<br> Do so below. be sure to add the trailing / or \ system depending of course</td>
+                    <td><label for="static" >If you would like to set a static location to put all the HTML output files,<br> Do so below. be sure to add the trailing /</label></td>
                 </tr>
                 <tr>
-                    <td><input class="inputtext" type="text" name="static" value="<?php if(file_exists('Config.cfg')) { $path=file_get_contents('Config.cfg'); echo $path;}else{echo getcwd();}?>"></td>
+                    <td><input class="form-control input-small" type="text" name="static" value="<?php if(file_exists('Config.cfg')) { $path=file_get_contents('Config.cfg'); echo $path;}else{echo getcwd();}?>"></td>
                 </tr>
                 <tr>
-                    <td><input type="submit" name="setDir" value="Save" /></td>
+                    <td><input class="btn btn-default btn-small" type="submit" name="setDir" value="Save" /></td>
                 </tr>
             </table>
         </form>
